5d10b9abb091e36d216358ad9c9511425fe0b0d2
MinorN <1481700559@qq.com>
2022-02-26 20:13:24 +0800

    添加全局样式和全局变量

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,19 +1,18 @@
-/* eslint-disable */
 <template>
-  <div class="nav">
-    <router-link to="/money">
+  <nav>
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="money"/>
       记账
     </router-link>
-    <router-link to="/labels">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="label"/>
       标签
     </router-link>
-    <router-link to="/statistics">
-      <Icon name="Statistics"/>
+    <router-link to="/statistics" class="item" active-class="selected">
+      <Icon name="statistics"/>
       统计
     </router-link>
-  </div>
+  </nav>
 </template>
 
 <script lang="ts">
@@ -26,7 +25,27 @@ export default class Nav extends Vue {
 </script>
 
 <style lang="scss" scoped>
-.nav {
-  border: 1px solid red;
+@import "src/assets/style/helper";
+nav {
+  display: flex;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+  font-size: 12px;
+
+  > .item {
+    width: 33.333333%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 2px 0;
+
+    > .icon {
+      width: 32px;
+      height: 32px;
+    }
+  }
+  >.item.selected{
+    color:$color-highlight
+  }
 }
 </style>
