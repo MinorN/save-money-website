49601266896e0689edc9646db73bc9d4e654517d
MinorN <1481700559@qq.com>
2022-02-26 12:53:53 +0800

    添加了导航栏

---
+++ src/views/Labels.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    labels.vue
+    <Nav/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+@Component
+export default class Labels extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Money.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    money.vue
+    <Nav/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+@Component
+export default class Money extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
