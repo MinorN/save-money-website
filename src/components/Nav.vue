fc7a977e130a13cb8bc4ea24c9ee8c453509ef80
MinorN <1481700559@qq.com>
2022-02-26 15:49:35 +0800

    创建Icon，并且import一个目录

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,15 +1,24 @@
+/* eslint-disable */
 <template>
   <div class="nav">
-    <router-link to="/money">记账</router-link>|
-    <router-link to="/labels">标签</router-link>|
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/money">
+      <Icon name="money"/>
+      记账
+    </router-link>
+    <router-link to="/labels">
+      <Icon name="label"/>
+      标签
+    </router-link>
+    <router-link to="/statistics">
+      <Icon name="Statistics"/>
+      统计
+    </router-link>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
-
 @Component
 export default class Nav extends Vue {
 
@@ -17,7 +26,7 @@ export default class Nav extends Vue {
 </script>
 
 <style lang="scss" scoped>
-.nav{
-  border:1px solid red;
+.nav {
+  border: 1px solid red;
 }
 </style>
