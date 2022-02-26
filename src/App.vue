5ada9942918df1952063138bf0f7adf395299f8a
MinorN <1481700559@qq.com>
2022-02-26 13:07:18 +0800

    Money页面布局

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,12 @@
 </template>
 
 <style lang="scss">
+* {
+  margin: 0;
+  box-sizing: border-box;
+  padding: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    money.vue
+  <div class="nav-wrapper">
+    <div class="content">
+      money.vue
+    </div>
     <Nav/>
   </div>
 </template>
@@ -8,6 +10,7 @@
 <script lang="ts">
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
+
 @Component
 export default class Money extends Vue {
 
@@ -15,5 +18,13 @@ export default class Money extends Vue {
 </script>
 
 <style lang="scss" scoped>
-
+.nav-wrapper {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  .content{
+    overflow: auto;
+    flex-grow: 1;
+  }
+}
 </style>
